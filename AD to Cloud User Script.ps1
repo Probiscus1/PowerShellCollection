918325c327f715160281d6c89a0d9a3f148a6c98
Probiscus1 <55387343+Probiscus1@users.noreply.github.com>
2020-08-18 16:31:16 +1000

    Add files via upload

---
+++ AD to Cloud User Script.ps1
@@ -0,0 +1,21 @@
+﻿$username = Read-Host -Prompt 'Enter the Username of the User you wish to move within AD'
+$restoreUPN = Read-Host -Prompt 'Enter the UPN of the User you wish to restore'
+
+echo 'Connecting to MSOL Service'
+Connect-MsolService
+echo 'Moving $username'
+get-aduser $Username | move-adobject -targetpath "OU=Users - Non-365,OU=SBSUsers,OU=Users,OU=MyBusiness,DC=hrrg,DC=local"
+echo 'Running AAD Connector Delta Sync'
+Start-ADSyncSyncCycle -PolicyType Delta
+echo 'Connecting to MSOL Service'
+echo 'Waiting 60 seconds for Delta Sync to complete...'
+Start-Sleep -s 30
+echo '30 seconds remaining...'
+Start-Sleep -s 20
+echo '10 seconds remaining...'
+Start-Sleep -s 10
+echo 'Wait Complete'
+echo 'Restoring $restoreUPN'
+Restore-MsolUser -UserPrincipalName $restoreUPN
+echo 'Restore command completed.'
+pause
---
+++ Password Expiry Check v1.2 - Blank.ps1
@@ -0,0 +1,23 @@
+﻿$DaysForward = 7
+$DaysBack = -14
+$Exchange = 
+$From = 
+$To = 
+$DestinationFile = "C:\temp\Password Expiry.txt"
+$StartDate1 = Get-date
+$EndDate1 = $StartDate1.AddDays($DaysForward)
+$StartDate2 = $StartDate1.AddDays($DaysBack)
+$EndDate2 = $StartDate1
+
+# Written by Nathan Reed from saberVox Cloud Solutions (1300 788 515).
+
+# Find AD passwords that are expiring within 7 days then output to $DestinationFile.
+echo  "Passwords expiring within 7 days:" > $DestinationFile
+Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Sort-Object ExpiryDate | Where-Object {$_.ExpiryDate -ge $StartDate1 -and $_.ExpiryDate -lt $EndDate1 } >> $DestinationFile
+
+# Find AD passwords that have expired within the last 14 days and have not been changed then output to $DestinationFile.
+echo "Expired passwords that have not been changed within the past 14 days:" >> $DestinationFile
+Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Sort-Object ExpiryDate | Where-Object {$_.ExpiryDate -ge $StartDate2 -and $_.ExpiryDate -lt $EndDate2 } >> $DestinationFile
+
+# Send $DestinationFile to $To via email.
+Send-MailMessage –From $From –To $To –Subject "Password Expiries" –Body “Attached is a list of Users with AD passwords expiring this week and expired passwords that have not been changed within the past 14 days." -Attachment $DestinationFile -SmtpServer $Exchange -Port 25
