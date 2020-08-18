14688e2b6e621d342ae33089aa2eb768e5387ac3
Probiscus1 <55387343+Probiscus1@users.noreply.github.com>
2020-08-18 16:32:25 +1000

    Update AD to Cloud User Script.ps1

--- AD to Cloud User Script.ps1
+++ AD to Cloud User Script.ps1
@@ -1,13 +1,11 @@
-﻿$username = Read-Host -Prompt 'Enter the Username of the User you wish to move within AD'
-$restoreUPN = Read-Host -Prompt 'Enter the UPN of the User you wish to restore'
+$username = Read-Host -Prompt 'Enter the Username of the User you wish to move within AD'
+$UPN = Read-Host -Prompt 'Enter the UPN of the User you wish to restore'
 
-echo 'Connecting to MSOL Service'
-Connect-MsolService
 echo 'Moving $username'
 get-aduser $Username | move-adobject -targetpath "OU=Users - Non-365,OU=SBSUsers,OU=Users,OU=MyBusiness,DC=hrrg,DC=local"
+echo 'Connecting to MSOL Service'
 echo 'Running AAD Connector Delta Sync'
 Start-ADSyncSyncCycle -PolicyType Delta
-echo 'Connecting to MSOL Service'
 echo 'Waiting 60 seconds for Delta Sync to complete...'
 Start-Sleep -s 30
 echo '30 seconds remaining...'
@@ -15,7 +13,10 @@ Start-Sleep -s 20
 echo '10 seconds remaining...'
 Start-Sleep -s 10
 echo 'Wait Complete'
-echo 'Restoring $restoreUPN'
-Restore-MsolUser -UserPrincipalName $restoreUPN
+echo 'Restoring $UPN'
+Restore-MsolUser -UserPrincipalName $UPN
 echo 'Restore command completed.'
+echo 'Setting ImmutableID to $null'
+Set-MSOLUser -UserPrincipalName $UPN -ImmutableID "$null"
+echo 'ImmutableID set'
 pause
